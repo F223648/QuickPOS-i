1a9a03b41f73c0f428ad4ff2bdfaa56781f174a5
Ibraheem <155681864+F223648@users.noreply.github.com>
2025-11-29 20:50:04 +0500

    ..

--- index.php
+++ index.php
@@ -1,3 +1,18 @@
+
+<header>
+  <div class="logo">QuickPOS</div>
+  <nav>
+    <ul>
+      <li><a href="#features">Features</a></li>
+      <li><a href="#pricing">Pricing</a></li>
+      <li><a href="#contact">Contact</a></li>
+    </ul>
+  </nav>
+  <a href="#signup" class="btn-signup">Sign Up</a>
+</header>
+
+
+
 <section id="features">
   <div class="feature">
     <img src="images/inventory-icon.png" alt="Inventory">

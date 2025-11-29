f7c8e38009b6e0ce61b5475d3fdb23bf15042e40
ibraheem <ibraheemshahidakaibi@gmail.com>
2025-11-29 17:21:16 +0500

    [POS-101] Add header

---
+++ index.php
@@ -0,0 +1,11 @@
+<header>
+  <div class="logo">QuickPOS</div>
+  <nav>
+    <ul>
+      <li><a href="#features">Features</a></li>
+      <li><a href="#pricing">Pricing</a></li>
+      <li><a href="#contact">Contact</a></li>
+    </ul>
+  </nav>
+  <a href="#signup" class="btn-signup">Sign Up</a>
+</header>
